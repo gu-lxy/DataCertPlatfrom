53cc63fab3efba63447652b703921623faa55dfe
unknown <gu-lxy>
2020-10-11 21:57:04 +0800

    Electronic data authentication platform

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"DataCertPlatfrom/db_mysql"
 	_ "DataCertPlatfrom/routers"
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
@@ -10,10 +11,13 @@ func main() {
 	//连接数据库
 	db_mysql.Connect()
 	//静态资源文件映射设置
-	beego.SetStaticPath("/js",".static/js")
-	beego.SetStaticPath("/css","./static/css")
+	fmt.Println("hello")
+	beego.SetStaticPath("/js","./static/js")
 	beego.SetStaticPath("/img","./static/img")
+	beego.SetStaticPath("/css","./static/css")
+	fmt.Println("hello")
 
 	beego.Run()
+	fmt.Println("hello")
 }
 
