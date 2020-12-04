a11e7ee20f2931c60a1c1e77b065460eda9c9f33
unknown <QQ2421282537@OUTLOOK.COM>
2020-12-04 22:56:58 +0800

    DataCertPlatForm

--- controllers/default.go
+++ controllers/default.go
@@ -11,7 +11,8 @@ type MainController struct {
 /**
  * 展示默认的首页：即用户注册页面
  */
-func (c *MainController) Get() {
-	//c.Ctx.WriteString("恭喜你，通过云服务器的公网ip访问到了我！")
-	c.TplName = "register.html
-}
+//func (c *MainController) Get() {
+//	//c.Ctx.WriteString("恭喜你，通过云服务器的公网ip访问到了我！")
+//	c.TplName = "register.html
+//}
+
